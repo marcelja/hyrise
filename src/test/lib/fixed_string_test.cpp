b32867d6d5df7fe8f8905dab91437e801bff2bed
bjennware <benjamin-feldmann@web.de>
2018-01-13 20:08:27 +0100

    add fixed string test

--- src/test/lib/fixed_string_test.cpp
+++ src/test/lib/fixed_string_test.cpp
@@ -1,79 +1,74 @@
 #include <memory>
 
-#include "../../base_test.hpp"
+#include "../base_test.hpp"
 #include "gtest/gtest.h"
 
-// #include "import_export/binary.hpp"
-// #include "operators/export_binary.hpp"
-// #include "operators/table_scan.hpp"
-// #include "operators/table_wrapper.hpp"
-// #include "storage/storage_manager.hpp"
-// #include "storage/value_vector.hpp"
-// #include "types.hpp"
-#include "../lib/storage/value_vector.hpp"
 #include "../lib/fixed_string.hpp"
 
 
 namespace opossum {
 
-class ValueVectorTest : public BaseTest {
+class FixedStringTest : public BaseTest {
  protected:
   void SetUp() override {}
 };
 
-TEST_F(ValueVectorTest, FirstExample) {
-  // uint8_t string_length = 10;
-  // pmr_concurrent_vector<int> test111;
-  // test111.push_back(123);
-  // test111.push_back(14);
-  // test111.push_back(6);
-  // std::cout << test111[0];
-
-  ValueVector<int> _values;
-  _values.push_back(27);
-
+TEST_F(FixedStringTest, InitFixedString) {
   char mem[] = {'f', 'o', 'o'};
   FixedString s(mem, 3);
   FixedString s2("bar");
   FixedString s3(mem, 2);
-  FixedString s4(s2);default
-  std::cout << s << std::endl;
-  std::cout << s2.string() << std::endl;
-  std::cout << s2 << std::endl;
-  std::cout << s3 << std::endl;
-  std::cout << s3.string() << std::endl;
-  std::cout << s4 << std::endl;
+  FixedString s4(s2);
 
   EXPECT_EQ(1, 1);
 }
 
-TEST_F(ValueVectorTest, asdfasdfsadf) {
+TEST_F(FixedStringTest, ToString) {
+  char mem[] = {'f', 'o', 'o'};
+  FixedString s(mem, 3);
+  FixedString s2("bar");
 
-  auto str1 = FixedString(std::string("askjjklasfkl"));
-  std::cout << str1.size();
-
-  EXPECT_EQ(str1.size(), size_t(12));
+  EXPECT_EQ(s.string(), "foo");
+  EXPECT_EQ(s2.string(), "bar");
 }
 
-TEST_F(ValueVectorTest, asdfasdfsalkasdfljkafsdjkldf) {
+TEST_F(FixedStringTest, SizeTest) {
+
+  FixedString str1("askjjklasfkl");
+
+  EXPECT_EQ(str1.size(), 12u);
+}
+
+TEST_F(FixedStringTest, LengthTest) {
 
   std::vector<char> test123 {'a', 'b', 'c', 'f'};
 
-  auto str1 = FixedString(&test123[0], 3);
-  std::cout << str1.string();
+  FixedString str1(&test123[0], 3);
 
   EXPECT_EQ(str1.string(), "abc");
 }
 
-TEST_F(ValueVectorTest, comparestrings) {
+TEST_F(FixedStringTest, CompareStrings) {
 
   std::vector<char> test123 {'a', 'b', 'c', 'f'};
   std::vector<char> test456 {'b', 'b', 'c', 'f'};
 
-  auto str1 = FixedString(&test123[0], 3);
-  auto str2 = FixedString(&test456[0], 4);
+  FixedString str1(&test123[0], 3);
+  FixedString str2(&test456[0], 4);
 
   EXPECT_TRUE(str1 < str2);
+  EXPECT_FALSE(str2 < str1);
+}
+
+TEST_F(FixedStringTest, EqualStrings) {
+  FixedString str1("foo");
+  FixedString str2("bar");
+  std::vector<char> testString {'f', 'o', 'o'};
+  FixedString str3(&testString[0], 3);
+
+  EXPECT_FALSE(str1.string().compare(str2.string()) == 0);
+  EXPECT_FALSE(str2.string().compare(str3.string()) == 0);
+  EXPECT_TRUE(str1.string().compare(str3.string()) == 0);
 }
 
 }  // namespace opossum
